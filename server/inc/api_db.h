02ae943086654baec52a53791ea669ac45264b6f
Mykhailo Burenko <mburenko@c1r10p9.khpi.ucode-connect.study>
2021-02-04 18:53:01 +0200

    add prototypes fun sql

--- server/inc/api_db.h
+++ server/inc/api_db.h
@@ -19,5 +19,9 @@ int dbRequest(sqlite3 *db, const char *sql);
 
 char *insertUsersSQL(User usr);
 char *insertMessageSQL(Message msg);//INSERT INTO Messages(id, idUser, Message, time, edit) VALUES (0,0,'Hello, Ucode!',1612451993,false)
+char *updateMessage(int id,char *newMessage);//UPDATE Messages set edit = true,Message = 'Hello, Ucode! I can edit.'where id = 0
+char *updatePassword(int id,char *newPassword);//UPDATE Users set password = '321' where id = 0
+char *deleteMessage(int id); // DELETE from Messages where id = 0
+
 
 #endif //OWN_UCHAT_API_DB_H
